3767c39323784703032d51e6e27e30090b264bbd
KritikaDas6 <daskritika.2625@gmail.com>
2023-07-14 23:44:50 -0700

    // working bar graph of #burnedacres vs firename

--- datastructures/data.py
+++ datastructures/data.py
@@ -1,8 +1,23 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-#to read the csv file
+
+#TO READ CSV
 data= pd.read_csv('datastructures/fireData.csv')
-data.describe()
 
+#print(data.describe()) #provides mean,count,std,min,25,50,75,max
 #print(data.head()) #prints first few tables
+
+x_values = data['FIRE_NAME']
+y_values = data['GIS_ACRES']
+
+#THIS CREATES THE PLOT
+plt.bar(x_values, y_values)
+
+plt.title('Fires burnt')
+plt.xlabel('Fire name')
+plt.ylabel('# of burned acres')
+
+#display graph
+plt.show()
