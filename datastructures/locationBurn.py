d783a20a3a65c9b8e4da0d6db59ae74526d9d7b8
KritikaDas6 <daskritika.2625@gmail.com>
2023-07-15 10:26:38 -0700

    dictorinary of all fire burnt acres/loc

--- datastructures/locationBurn.py
+++ datastructures/locationBurn.py
@@ -1,45 +1,32 @@
 import pandas as pd
+import pandas as pd
 
-#--------------------------------------
-#aggregate data
-#step 1: make categories
-
-# totalacres=np.array()
-# acrecount=0
-
-# def locAcre(file_name, column1, column 2)
-# for name1 in data['UNIT_ID']:
-#     for names in data['UNIT_ID']:
-#         if name1==names:
-            
-
-#     if name==
-# if data['UNIT_ID']
-#-------------------------------------
-
-
-# def calculate_total_acre(file_path, name_column, acre_column):
+def calculate_total_acre(file_path, name_column, acre_column):
     # Read the CSV file
-data = pd.read_csv('datastructures/fireData.csv')
-# print(data.head())
-print(3)
+    data = pd.read_csv('datastructures/fireData.csv')
 
     # Create a dictionary to store the total acreage for each name
-total_acreage = {}
+    total_acreage = {}
 
     # Iterate over the rows
-for index, row in data.iterrows():
-    name = row[data['UNIT_ID']]
-    acre = row[data['GIS_ACRES']]
+    for index, row in data.iterrows():
+        name = row['UNIT_ID']
+        acre = row['GIS_ACRES']
 
         # Check if the name exists in the dictionary
-    if name in total_acreage:
+        if name in total_acreage:
             # Add the acreage to the existing total
-        total_acreage[name] += acre
-    else:
+            total_acreage[name] += acre
+        else:
             # Add a new entry in the dictionary for the name
-        total_acreage[name] = acre
+            total_acreage[name] = acre
 
     # Print the total acreage for each name
-for name, acre in total_acreage.items():
-    print(name, ":", acre)
+    for name, acre in total_acreage.items():
+        print(name, ":", acre)
+
+# Call the function with the appropriate arguments
+calculate_total_acre('fireData.csv', 'Name', 'Acre')
+
+
+    # (santaclar:60, santaclara:60, tuscane:80)
