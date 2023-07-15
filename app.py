b486578c8c34a5f2a9d706c41417e21619e77788
Ritika Gupta <ritikag.work06@gmail.com>
2023-07-15 11:08:56 -0700

    fixed flask and added drought data

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/droughts')
+def droughts():
+    return render_template('droughts.html')
+
+@app.route('/fires')
+def fires():
+    return render_template('fires.html')
+
+if __name__ == '__main__':
+    app.run()
