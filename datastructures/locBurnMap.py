222e0a2b7c893a25c0e5159a5d41835160478338
KritikaDas6 <daskritika.2625@gmail.com>
2023-07-15 22:57:37 -0700

    this correct longitude an dlatitude correctly

--- datastructures/locBurnMap.py
+++ datastructures/locBurnMap.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('datastructures/fireData1.csv')
 
+
 # Assuming 'california.shp' is the shapefile for California
 california = gpd.read_file('datastructures/map/cali.shp')
 
 # Create a GeoSeries with the same length as the DataFrame
-geometry = gpd.points_from_xy(data['LONGITUDE'], data['LATITUDE'])
+geometry = gpd.points_from_xy(data['LATITUDE'], data['LONGITUDE'])
 
 # Create a GeoDataFrame with the GeoSeries as the geometry
 gdf = gpd.GeoDataFrame(data, geometry=geometry)
@@ -17,7 +18,7 @@ gdf = gpd.GeoDataFrame(data, geometry=geometry)
 california.plot(color='lightgray', edgecolor='black', figsize=(10, 8))
 
 # Plot the bubbles
-gdf.plot(ax=plt.gca(), marker='o', color='red', markersize=data['BUBBLESIZE'], alpha=0.7)
+gdf.plot(ax=plt.gca(), marker='o', color='red', markersize=data['GIS_ACRES'], alpha=0.7)
 
 plt.title('Bubble Map of California')
 plt.xlabel('Longitude')
